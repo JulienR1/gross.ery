ee768122da7abd4e1349524e03b27496d7504395
Julien Rousseau <julrousseau20@gmail.com>
2021-09-05 01:18:52 -0400

    CloudService default route

--- cloudService/src/index.ts
+++ cloudService/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+import cors from "cors";
+import express,{Request,Response} from "express";
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req:Request,res:Response)=>{
+    res.sendStatus(200);
+});
+
+app.listen(port,()=>console.log(`Listening on port ${port}.`));
