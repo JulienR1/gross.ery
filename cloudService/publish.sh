2373a7fb1d1cec2dfddb8cf71f5a8d6a155e9614
Julien Rousseau <julrousseau20@gmail.com>
2021-09-05 09:59:41 -0400

    Updated publish script

--- cloudService/publish.sh
+++ cloudService/publish.sh
@@ -3,5 +3,8 @@
 git init
 git add .
 git commit -am "automatic publish"
-git push https://git.heroku.com/grossery.git master
+git push https://git.heroku.com/grossery.git master --force
 rm -rf .git
+
+ read -s -n 1 -p "Press any key to continue . . ."
+ echo ""
