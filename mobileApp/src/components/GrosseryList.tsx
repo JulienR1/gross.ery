a4f84e8e56285ed44c685fb4dc32aa4ef6c0cf12
Julien Rousseau <julrousseau20@gmail.com>
2021-09-11 14:00:15 -0400

    Data fetching from server and basic rendering

--- mobileApp/src/components/GrosseryList.tsx
+++ mobileApp/src/components/GrosseryList.tsx
@@ -1,6 +1,8 @@
-import React, {useEffect} from 'react';
-import {Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, View} from 'react-native';
 import {IListParams} from '../models/NavigationParams';
+import {SERVER_ENDPOINT} from '@env';
+import {FlatList, TouchableOpacity} from 'react-native-gesture-handler';
 
 interface IProps {
   route: INavigationRoute;
@@ -10,15 +12,59 @@ interface INavigationRoute {
   params: IListParams;
 }
 
+interface ListData {
+  _id: string;
+  items: ItemData[];
+}
+
+interface ItemData {
+  name: string;
+  checked: boolean;
+}
+
 export function GrosseryList({route}: IProps) {
-  const server = process.env.REACT_APP_SERVER_ENDPOINT as string;
+  const [items, setItems] = useState<ItemData[]>([]);
 
   useEffect(() => {
-    fetch(`${server}/${route.params.listId}`).then(response => {
-      response.json().then(content => {
-        console.log(content);
-      });
+    fetchListData(route.params.listId).then(listData => {
+      setItems(listData.items);
+      updateLocalStorage(listData._id, listData.items.length);
     });
   }, []);
-  return <Text>{route.params.listId}</Text>;
+
+  const fetchListData = (listId: string): Promise<ListData> => {
+    return new Promise(async resolve => {
+      const response = await fetch(`${SERVER_ENDPOINT}?${listId}`);
+      const jsonData = await response.json();
+      return resolve(jsonData);
+    });
+  };
+
+  const updateLocalStorage = (id: string, itemCount: number) => {
+    const lastUpdateTime = new Date();
+    console.log('Saving to local storage.. TODO');
+  };
+
+  return (
+    <>
+      {items?.length === 0 && <Text>Loading!</Text>}
+      {items?.length !== 0 && (
+        <View>
+          <Text>Nom de la liste ici</Text>
+          <FlatList
+            data={items}
+            keyExtractor={(_, index) => index.toString()}
+            renderItem={({item}: {item: ItemData}) => (
+              <View>
+                <Text>{item.name}</Text>
+                <TouchableOpacity>
+                  <Text> {item.checked ? 'checked' : 'not checked'}</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          />
+        </View>
+      )}
+    </>
+  );
 }
