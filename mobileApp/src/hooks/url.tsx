aae2e031f22dd7b6a278baac77fcc83d0e032103
Julien Rousseau <julrousseau20@gmail.com>
2022-03-09 23:39:35 -0500

    Parsing from custom scheme attempt

--- mobileApp/src/hooks/url.tsx
+++ mobileApp/src/hooks/url.tsx
@@ -2,9 +2,19 @@ import {useEffect, useState} from 'react';
 import {Linking} from 'react-native';
 
 export function useUrl() {
+  const CUSTOM_SCHEME = 'grossery://';
   const [url, setUrl] = useState<string | undefined>(undefined);
 
-  const updateUrl = (url: string | null) => setUrl(url ?? undefined);
+  const updateUrl = (url: string | null) => {
+    const fromSchemeUrlStart = `${CUSTOM_SCHEME}?target_url=`;
+    if (url?.startsWith(fromSchemeUrlStart)) {
+      const parsedRegularUrl = url.replace(fromSchemeUrlStart, '');
+      const regularUrl = decodeURIComponent(parsedRegularUrl);
+      setUrl(regularUrl);
+    } else {
+      setUrl(url ?? undefined);
+    }
+  };
 
   useEffect(() => {
     Linking.getInitialURL().then(url => updateUrl(url));
@@ -14,6 +24,10 @@ export function useUrl() {
 
     return () => listener.remove();
   }, []);
+
+  useEffect(() => {
+    console.log(url);
+  }, [url]);
 
   return url;
 }
