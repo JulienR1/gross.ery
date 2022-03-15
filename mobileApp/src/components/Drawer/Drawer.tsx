d71903bbc01e1252962bf29f7dfa44d03a8ae106
Julien Rousseau <julrousseau20@gmail.com>
2022-03-15 00:34:13 -0400

    Added animation on drawer

--- mobileApp/src/components/Drawer/Drawer.tsx
+++ mobileApp/src/components/Drawer/Drawer.tsx
@@ -1,6 +1,6 @@
 import {useNavigation} from '@react-navigation/core';
-import React, {ReactNode, ReactNodeArray, useEffect} from 'react';
-import {View} from 'react-native';
+import React, {ReactNode, ReactNodeArray, useEffect, useState} from 'react';
+import {Animated, View} from 'react-native';
 import {styles} from './styles';
 
 interface IProps {
@@ -10,6 +10,17 @@ interface IProps {
 
 export function Drawer({children, onClose}: IProps) {
   const navigation = useNavigation();
+  const [animationPercent, setAnimationPercent] = useState(0);
+
+  useEffect(() => {
+    const animation = new Animated.Value(0);
+    Animated.timing(animation, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+    animation.addListener(({value}) => setAnimationPercent(value));
+  }, []);
 
   useEffect(() => {
     const closeDrawerBeforeRemove = (event: any) => {
@@ -26,7 +37,13 @@ export function Drawer({children, onClose}: IProps) {
   return (
     <View style={styles.backdrop} onTouchStart={onClose}>
       <View
-        style={styles.container}
+        style={[
+          styles.container,
+          {
+            transform: [{translateY: 200 * (1 - animationPercent)}],
+            opacity: animationPercent,
+          },
+        ]}
         onTouchStart={event => {
           event.stopPropagation();
         }}>
