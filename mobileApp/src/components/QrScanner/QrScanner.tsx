404dcb32582009ff0bbc8c7e581511a78e913b51
Julien Rousseau <julrousseau20@gmail.com>
2022-03-16 00:38:26 -0400

    Managed error message on camera permission denial

--- mobileApp/src/components/QrScanner/QrScanner.tsx
+++ mobileApp/src/components/QrScanner/QrScanner.tsx
@@ -1,10 +1,16 @@
 import config from './../../config';
-import React from 'react';
-import {TouchableOpacity, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {PermissionsAndroid, Text, TouchableOpacity, View} from 'react-native';
 import {RNCamera, BarCodeReadEvent} from 'react-native-camera';
 import {Icon} from 'react-native-elements';
 import {Colors} from '../../styles/colors';
 import {styles} from './styles';
+
+enum PermissionStatus {
+  Granted = 'granted',
+  Denied = 'denied',
+  Loading = 'loading',
+}
 
 interface IProps {
   onListIdFound: (listId: string) => void;
@@ -12,6 +18,11 @@ interface IProps {
 }
 
 export function QrScanner({onListIdFound, onListNotFound}: IProps) {
+  const [isMounted, setIsMounted] = useState(true);
+  const [currentCameraPermission, setCurrentCameraPermission] = useState(
+    PermissionStatus.Loading,
+  );
+
   const onQrRead = (event: BarCodeReadEvent) => {
     const {data} = event;
 
@@ -21,9 +32,57 @@ export function QrScanner({onListIdFound, onListNotFound}: IProps) {
     }
   };
 
+  useEffect(() => {
+    const validatePermission = async () => {
+      const permission = 'android.permission.CAMERA';
+
+      let permissionGranted = await PermissionsAndroid.check(permission);
+      if (!permissionGranted) {
+        if (isMounted) {
+          setCurrentCameraPermission(PermissionStatus.Denied);
+        }
+
+        const grantStatus = await PermissionsAndroid.request(permission);
+        permissionGranted = grantStatus === 'granted';
+      }
+
+      if (isMounted) {
+        setCurrentCameraPermission(
+          permissionGranted
+            ? PermissionStatus.Granted
+            : PermissionStatus.Denied,
+        );
+      }
+    };
+
+    validatePermission();
+    return () => setIsMounted(false);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <RNCamera style={styles.scanner} onBarCodeRead={onQrRead} />
+      {currentCameraPermission === PermissionStatus.Denied && (
+        <View style={styles.noCameraContainer}>
+          <Icon
+            size={50}
+            color={Colors.White}
+            name="camera-off"
+            type="feather"
+          />
+          <Text style={styles.noCameraText}>
+            Impossible d'accéder à la caméra
+          </Text>
+        </View>
+      )}
+
+      {currentCameraPermission === PermissionStatus.Granted && (
+        <RNCamera
+          style={styles.scanner}
+          onBarCodeRead={onQrRead}
+          captureAudio={false}
+        />
+      )}
+
       <TouchableOpacity style={styles.closeIcon} onPress={onListNotFound}>
         <Icon name="close" size={35} color={Colors.White} />
       </TouchableOpacity>
