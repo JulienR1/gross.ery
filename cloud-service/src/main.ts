0f998bd66380d6e295258c8fc6e7f1a87b7713fe
Julien Rousseau <julrousseau20@gmail.com>
2022-03-23 10:29:43 -0400

    Initialized nestjs app

---
+++ cloud-service/src/main.ts
@@ -0,0 +1,11 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(5000);
+}
+bootstrap();
