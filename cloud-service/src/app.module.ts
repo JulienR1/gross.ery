c0c1bb7259b06ed0bcbb260f029ac2d18c2d5d5d
Julien Rousseau <julrousseau20@gmail.com>
2022-03-23 10:55:27 -0400

    Added validation code route

--- cloud-service/src/app.module.ts
+++ cloud-service/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ListModule } from './list/list.module';
 import { InfoModule } from './info/info.module';
+import { CodeModule } from './code/code.module';
 
 @Module({
-  imports: [ListModule, InfoModule],
+  imports: [ListModule, InfoModule, CodeModule],
 })
 export class AppModule {}
