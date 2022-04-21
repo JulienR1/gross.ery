0cb9d1d4452a310661e06d4ce62f5269ccaff6cd
Julien Rousseau <julrousseau20@gmail.com>
2022-04-21 14:55:37 -0400

    Added env configuration

---
+++ app/src/config/config.ts
@@ -0,0 +1,19 @@
+import { config as dev } from './env.dev';
+import { config as prod } from './env.prod';
+
+export interface VariableConfig {
+  IS_PROD: boolean;
+  SERVER_URL: string;
+}
+
+export interface Config extends VariableConfig {
+  QR_PREFIX: string;
+}
+
+const env = process.env.NODE_ENV;
+const variableConfig = env === 'production' ? prod : dev;
+
+export const config: Config = {
+  ...variableConfig,
+  QR_PREFIX: 'gross.ery',
+};
---
+++ app/src/config/env.template.ts
@@ -0,0 +1,6 @@
+import { VariableConfig } from './config';
+
+export const config: VariableConfig = {
+  SERVER_URL: '',
+  IS_PROD: false,
+};
